706b493258a26f10d30e253f5434e9fb8052dea2
b0kch01 <nath49ers@gmail.com>
2021-08-19 10:15:17 -0700

    what is this??

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,17 +1,28 @@
+console.log("🥲 Trna be hackerman??");
 lax.init();
 
 lax.addDriver('scrollY', function() {
-    return window.scrollY
+    return window.scrollY;
 }, {
+    easing: "easeInOutQuint",
     frameStep: 0.5
-})
+});
 
 
 // Hero animation
 
-const OFFSET = -250
-const BASE_MOVEMENT = -70
-const STAGGER = 30
+const OFFSET = -250;
+const BASE_MOVEMENT = -70;
+const STAGGER = 30;
+
+lax.addElements('.slide-right', {
+    scrollY: {
+        translateX: [
+            ["elInY", "elCenterY"],
+            [100, 0]
+        ]
+    }
+});
 
 lax.addElements('.slide-up', {
     scrollY: {
@@ -20,7 +31,7 @@ lax.addElements('.slide-up', {
             [OFFSET, BASE_MOVEMENT]
         ]
     }
-})
+});
 
 lax.addElements('.slide-up-alt1', {
     scrollY: {
@@ -29,7 +40,7 @@ lax.addElements('.slide-up-alt1', {
             [OFFSET, BASE_MOVEMENT + STAGGER]
         ]
     }
-})
+});
 
 lax.addElements('.slide-up-alt2', {
     scrollY: {
@@ -38,4 +49,4 @@ lax.addElements('.slide-up-alt2', {
             [OFFSET, BASE_MOVEMENT + 2 * STAGGER]
         ]
     }
-})
+});
